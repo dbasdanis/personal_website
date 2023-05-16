8230fa9f2b042e5040b43086ab8bf162ebad16ac
dbasdanis <dbasdanis97@gmail.com>
2023-05-16 12:58:13 +0200

    feat: add logo to navbar

--- personal_website/src/components/Navbar.jsx
+++ personal_website/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { useState } from 'react'
+
+import { close, logo, menu} from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
+    </nav>
   )
 }
 
