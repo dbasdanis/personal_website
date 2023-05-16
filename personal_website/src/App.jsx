f8600e5f01d42d3fa600fcd1248ccb548ba93249
dbasdanis <dbasdanis97@gmail.com>
2023-05-16 11:54:03 +0200

    feat: start App.jsx

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div >
+
+    </div>
+  </div>
+)
+
 
 export default App
