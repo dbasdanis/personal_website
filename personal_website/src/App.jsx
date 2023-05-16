af1fb2bfe9d4e00e308f6b2f405234734b4087b9
dbasdanis <dbasdanis97@gmail.com>
2023-05-16 12:18:32 +0200

    feat: add to App.jsx

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -6,6 +6,25 @@ const App = () => (
         Navbar
       </div>
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
   </div>
 )
 
