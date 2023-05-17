1bffcc549e76ceb18fb301b40c203922db7d636e
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 13:00:50 +0200

    feat: add text below the header

--- personal_website/src/components/Hero.jsx
+++ personal_website/src/components/Hero.jsx
@@ -17,12 +17,16 @@ const Hero = () => (
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="sm:block hidden"/> {" "}
           <span className="text-gradient">Generation</span> {" "}
-          Payment Method.
         </h1>
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted />
         </div>
       </div>
+
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
     </div>
   </section>
 )
