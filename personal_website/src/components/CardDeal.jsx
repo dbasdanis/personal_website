e463fb790933a38a4a3fa3469d12a9dd41a2ebf4
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 18:22:24 +0200

    feat: add text for CardDeal section

--- personal_website/src/components/CardDeal.jsx
+++ personal_website/src/components/CardDeal.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style'; 
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden"/> in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+       Elit enim sed massa etiam, Mauris eu adipiscing ultices ametodio aenean neque. Fusce ipsum orci rhomcus aliporttitor integer platea placerat.
+      </p>
+      <Button styles="mt-10"/> 
+    </div>
+
+  </section>
+)
 
 export default CardDeal
