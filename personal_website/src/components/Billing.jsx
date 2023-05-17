9b522dde1b1d511f60ea91ff63544a41f5f5e536
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 18:54:54 +0200

    fix: remove gradient from billing

--- personal_website/src/components/Billing.jsx
+++ personal_website/src/components/Billing.jsx
@@ -6,8 +6,6 @@ const Billing = () => (
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
     
-      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"></div>
-      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient"></div>
     </div>
 
     <div className={layout.sectionInfo}>
