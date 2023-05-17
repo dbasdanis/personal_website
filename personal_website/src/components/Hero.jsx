683f9e420074ee7b644bf83dadbbd6c0da16b133
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 15:31:50 +0200

    feat: changes on Hero section

--- personal_website/src/components/Hero.jsx
+++ personal_website/src/components/Hero.jsx
@@ -28,7 +28,8 @@ const Hero = () => (
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
     </div>
-    <div>
+
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
       <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"/>
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"/>
