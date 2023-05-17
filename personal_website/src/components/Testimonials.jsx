79f25ed5483553a6217ee49c2015e530c8925de1
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 19:09:37 +0200

    feat: text of testimonials

--- personal_website/src/components/Testimonials.jsx
+++ personal_website/src/components/Testimonials.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+import { feedback } from '../constants';
+import styles from '../style';
+import FeedbackCard from './FeedbackCard';
+ 
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    <div />
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h1 className={styles.heading2}>
+        What people are <br className="sm:block hidden"/> saying about us
+      </h1>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+  </section>
+)
 
 export default Testimonials
