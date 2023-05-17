7acd8b59f34cbb90d244e2cdc1eea42dd191f076
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 19:22:06 +0200

    feat: add gradient to testimonials

--- personal_website/src/components/Testimonials.jsx
+++ personal_website/src/components/Testimonials.jsx
@@ -4,7 +4,7 @@ import FeedbackCard from './FeedbackCard';
  
 const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-    <div />
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>
         What people are <br className="sm:block hidden"/> saying about us
