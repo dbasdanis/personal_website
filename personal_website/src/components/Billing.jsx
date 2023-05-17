0aa31dff85ab5392f0d23c0689212a50f955c780
dbasdanis <dbasdanis97@gmail.com>
2023-05-17 18:53:28 +0200

    fix: billing section

--- personal_website/src/components/Billing.jsx
+++ personal_website/src/components/Billing.jsx
@@ -7,13 +7,14 @@ const Billing = () => (
       <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
     
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"></div>
-      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient"></div>
     </div>
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden"/> billing & invoicing.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam, Mauris eu adipiscing ultices ametodio aenean neque. Fusce ipsum orci rhomcus aliporttitor integer platea placerat.
+        Elit enim sed massa etiam, Mauris eu adipiscing ultices ametodio 
+        aenean neque. Fusce ipsum orci rhomcus aliporttitor integer platea placerat.
       </p>
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
         <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
