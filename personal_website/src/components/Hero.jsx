a38662699c0425c29f8d066b947f0e8d10369382
dbasdanis <dbasdanis97@gmail.com>
2023-05-18 11:47:21 +0200

    feat: make hero section personal

--- personal_website/src/components/Hero.jsx
+++ personal_website/src/components/Hero.jsx
@@ -5,28 +5,32 @@ import GetStarted from './GetStarted';
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className={`flex flex-row items-center py-[16px] px-4 bg-discount-gradient rounded-[10px] mb-2`}>
+      {/* <div className={`flex flex-row items-center py-[16px] px-4 bg-discount-gradient rounded-[10px] mb-2`}>
         <img src={discount} alt="discount"  className="w-[32px] h-[32px]"/>
         <p className={`${styles.paragraph} ml-2`}>
           <span className="text-white">20%</span> Discount For {" "}
           <span className="text-white">1 Month</span> Account  
         </p>
-      </div>
+      </div> */}
 
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-          The Next <br className="sm:block hidden"/> {" "}
-          <span className="text-gradient">Generation</span> {" "}
+          Dionysios <br className="sm:block hidden"/> {" "}
+          <span className="text-gradient">Basdanis</span> {" "}
         </h1>
-        <div className="ss:flex hidden md:mr-4 mr-0">
+        {/* <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted />
-        </div>
+        </div> */}
       </div>
 
-      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+      {/* <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
         Payment Method.
-      </h1>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+      </h1> */}
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Highly skilled software engineer with experience in full stack development, implementing
+applications using Flask, Camunda and Appsmith. Deep knowledge of deployment using
+Kubernetes and Docker. Adept at writing C# applications, and working with databases in R, Weka
+and Python. Collaborative and productive with excellent planning and communication abilities.
+Well-disciplined, self-motivated and with great adaptability.</p>
     </div>
 
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
@@ -35,9 +39,9 @@ const Hero = () => (
       <div className="absolute z-[1] w-[60%] h-[60%] rounded-full bottom-20 white__gradient"/>
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"/>
     </div>
-    <div className={`ss:hidden ${styles.flexCenter}`}>
+    {/* <div className={`ss:hidden ${styles.flexCenter}`}>
       <GetStarted />
-    </div>
+    </div> */}
   </section>
 )
 
