f35c253100fe5612db9870527768fdc508e46226
dbasdanis <dbasdanis97@gmail.com>
2023-05-18 12:32:32 +0200

    feat: change navbar and footer

--- personal_website/src/components/Footer.jsx
+++ personal_website/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => (
         <img
           src={logo}
           alt="hoobank"
-          className="w-[100px] h-[72.14px] rounded"
+          className="w-[240px] h-[72.14px] rounded"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
           A new way to make the payments easy, reliable and secure.
@@ -41,7 +41,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+        Copyright Ⓒ 2023 Dioinysios Basdanis. All Rights Reserved.
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
