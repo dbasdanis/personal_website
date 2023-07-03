58b8431919336eff87e18500f5b58a17b27ed01d
dbasdanis <dbasdanis97@gmail.com>
2023-07-03 17:30:55 +0200

    feat: update educaton

--- personal_website/src/components/Billing.jsx
+++ personal_website/src/components/Billing.jsx
@@ -5,9 +5,9 @@ const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
 
     <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>University of Thessaly <br className="sm:block hidden"/></h2>
+      <h2 className={styles.heading2}>Education <br className="sm:block hidden"/></h2>
       <p className={`${styles.paragraph} max-w-[1000px] mt-5`}>
-        
+        <h3 className={styles.heading3}> • University of Thessaly</h3>
         Minor in Electrical and Computer Engineering. <br />
         • Cumulative GPA: 8.28 / 10.00<br />
         • Thesis work: Predictive Model of Motion Picture Success using Machine Learning Algorithms<br />
@@ -15,7 +15,13 @@ const Billing = () => (
             motion pictures.<br />
             - Achieved a grade of 10/10, for the thesis work, demonstrating exceptional research and<br />
             analytical skills.<br />
-        
+        • Developed projects in fields as: hardware programming, operating systems, parallel programming, <br />
+web development, networks, machine learning, databases, and algorithms. <br />
+          
+        <h3 className={styles.heading3}> • Music High School of Volos</h3>
+        • Cumulative GPA: 19.13 / 20.00 <br />
+        • Proficient in playing the accordion, and piano. <br />
+        • Received comprehensive training in musical theory, composition, and performance. <br />
       </p>
     </div>
     
