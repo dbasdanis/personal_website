4e90880812e8f46bf44393ccbc9758b39c38d49a
dbasdanis <dbasdanis97@gmail.com>
2023-07-03 17:16:29 +0200

    feat: update content

--- personal_website/src/components/Hero.jsx
+++ personal_website/src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => (
 
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-          Dionysios <br className="sm:block hidden"/> {" "}
+          Dionysios {" "}
           <span className="text-gradient">Basdanis</span> {" "}
         </h1>
       </div>
 
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Highly skilled software engineer with experience in full stack development, implementing
+      <p className={`${styles.paragraph} max-w-[800px] mt-5`}>Highly skilled software engineer with experience in full stack development, implementing
 applications using Flask, Camunda and Appsmith. Deep knowledge of deployment using
 Kubernetes and Docker. Adept at writing C# applications, and working with databases in R, Weka
 and Python. Collaborative and productive with excellent planning and communication abilities.
@@ -21,7 +21,7 @@ Well-disciplined, self-motivated and with great adaptability.</p>
     </div>
 
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-      <img src={robot} alt="billing" className="w-[80%] h-[100%] relative z-[5]"/>
+      <img src={robot} alt="billing" className="w-[230px] h-[300px] relative z-[5] rounded-full"/>
       <div className="absolute z-[0] w-[40%] h-[35%] bottom-20 pink__gradient"/>
       <div className="absolute z-[1] w-[60%] h-[60%] rounded-full bottom-20 white__gradient"/>
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"/>
