2277685d246bbaa592b04d95a588f11b973c74f5
dbasdanis <dbasdanis97@gmail.com>
2023-07-04 16:55:30 +0200

    fix: commits

--- personal_website/src/assets/index.js
+++ personal_website/src/assets/index.js
@@ -20,9 +20,6 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 
 export {
   airbnb,
@@ -47,7 +44,4 @@ export {
   instagram,
   linkedin,
   twitter,
-  people01,
-  people02,
-  people03,
 };
