4f00ac538fa2805d2a340cb9c581aeaca6474928
dbasdanis <dbasdanis97@gmail.com>
2023-07-04 17:04:34 +0200

    fix: bug with pictures

--- personal_website/src/assets/index.js
+++ personal_website/src/assets/index.js
@@ -20,6 +20,9 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
 
 export {
   airbnb,
@@ -44,4 +47,7 @@ export {
   instagram,
   linkedin,
   twitter,
+  people01,
+  people02,
+  people03,
 };
